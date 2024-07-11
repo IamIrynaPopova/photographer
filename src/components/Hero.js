3e70a93a0d3247f23ba8633689598e6af8592ada
Iryna Popova <popovaira969@gmail.com>
2024-07-12 01:25:01 +0300

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,18 +7,24 @@ const Hero = () => {
   return (
     <section className="hero container">
       <div className="hero__wrapper">
+        <img className="hero__image" src={hero} alt="human"></img>
         <div className="hero__title-wrapper">
-          <h1 className="hero__title">Jacob Grønberg</h1>
+          <h1 className="hero__title">
+            Jacob <br></br>Grønberg
+          </h1>
           <p className="hero__description">Photographer </p>
           <p className="hero__description">& Visual artist</p>
         </div>
-        <img className="hero__image" src={hero} alt="human"></img>
+        <div className="hero__button-wrapper">
+          <img
+            className="hero__button-image"
+            src={imageButton}
+            alt="human"
+          ></img>
+          <p className="hero__button-text">Hi, Im Jacob</p>
+          <Button text="Work With Me" />
+        </div>
       </div>
-      <div className="hero__button-wrapper">
-        <img className="hero__button-image" src={imageButton} alt="human"></img>
-        <p className="hero__button-text">Hi, Im Jacob</p>
-      </div>
-      <Button text="Work With Me" />
     </section>
   );
 };
