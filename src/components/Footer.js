f5d5effb61b48d968d0e5eddd4b6bdab90d63e3d
Iryna Popova <popovaira969@gmail.com>
2024-07-19 15:45:31 +0300

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Button from "./Button";
-import human from "../image/human.jpg";
 
 const Footer = () => {
   const circleStyle = {
@@ -29,7 +28,50 @@ const Footer = () => {
         <h2 className="footer__title">
           Let’s Work <span>Together</span>
         </h2>
-        <img src={human}></img>
+      </div>
+      <div className="footer__box">
+        <div className="footer__subtitle-wrapper">
+          <h3 className="footer__subtitle">Jacob Grønberg</h3>
+          <p className="footer__description">Photograph & Visual Artist</p>
+        </div>
+        <div className="footer__menu-wrapper">
+          <p className="footer__menu-text">Menu</p>
+          <ul className="footer__menu">
+            <li className="footer__menu-item">
+              <a className="footer__menu-link" href="">
+                Home
+              </a>
+            </li>
+            <li className="footer__menu-item">
+              <a className="footer__menu-link" href="">
+                Projects
+              </a>
+            </li>
+            <li className="footer__menu-item">
+              <a className="footer__menu-link" href="">
+                Exhibitions
+              </a>
+            </li>
+            <li className="footer__menu-item">
+              <a className="footer__menu-link" href="">
+                About
+              </a>
+            </li>
+            <li className="footer__menu-item">
+              <a className="footer__menu-link" href="">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__info-wrapper">
+          <p className="footer__info-text">Info</p>
+          <ul className="footer__info">
+            <li className="footer__info-item">Styleguide</li>
+            <li className="footer__info-item">Licensing</li>
+            <li className="footer__info-item">Changelog</li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
